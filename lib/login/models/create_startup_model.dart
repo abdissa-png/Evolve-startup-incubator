860166d541ea130c46c5802319e1ccbc9400c4a3
ebissa <114221753+ebissa@users.noreply.github.com>
2023-05-24 23:41:53 +0300

    Add files via upload

---
+++ lib/login/models/create_startup_model.dart
@@ -0,0 +1,24 @@
+class StartupModel {
+  final String email;
+  final String password;
+  final String companyName;
+  final String phoneNumber;
+  final String address;
+  final String founders;
+  final String additionalInfo;
+  final String assistanceRequired;
+  final String assistanceDetails;
+  final String companyStage;
+  StartupModel({
+    required this.email,
+    required this.password,
+    required this.companyName,
+    required this.phoneNumber,
+    required this.address,
+    required this.founders,
+    required this.additionalInfo,
+    required this.assistanceRequired,
+    required this.assistanceDetails,
+    required this.companyStage,
+  });
+}
