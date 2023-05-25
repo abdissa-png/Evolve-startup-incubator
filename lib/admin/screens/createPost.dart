57b7aa7757bbdcda9fccf2a6b83b4a3127cba6ed
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-05-25 07:00:22 +0000

    Add files via upload

---
+++ lib/admin/screens/createPost.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class CreatePost extends StatefulWidget {
+  const CreatePost({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePost> createState() => _CreatePostState();
+}
+
+class _CreatePostState extends State<CreatePost> {
+  final controler = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_rounded),
+            onPressed: (){
+              Navigator.pop(context);
+            },
+          ),
+          // leading: Icon(Icons.arrow_back_rounded),
+          backgroundColor: Colors.indigo[700],
+          title: const Text("Create Post"),
+        ),
+
+        body: // Generated code for this TextField Widget...
+        Column(
+          children: [Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(8, 0, 8, 0),
+            child: Container(
+
+              child: TextFormField(
+                controller: controler,
+
+                decoration: InputDecoration(
+                  labelText: 'Write Post',
+
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: const BorderSide(
+                      // color: FlutterFlowTheme.of(context).alternate,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: const BorderSide(
+                      // color: FlutterFlowTheme.of(context).primary,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: const BorderSide(
+                      // color: FlutterFlowTheme.of(context).error,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  focusedErrorBorder: UnderlineInputBorder(
+                    borderSide: const BorderSide(
+                      // color: FlutterFlowTheme.of(context).error,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+
+                maxLines: 15,
+
+              ),
+
+
+            ),
+
+          ),
+
+            SizedBox(
+              width: double.infinity,
+
+              child: ElevatedButton(
+
+                child: const Text(
+                    'Post',
+                  style: TextStyle(
+                    color: Colors.white,
+                    decorationColor: Colors.indigo
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ]
+        )
+      ),
+    );
+  }
+}
