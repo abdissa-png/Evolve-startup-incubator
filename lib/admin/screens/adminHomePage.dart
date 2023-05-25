657b92a4c20d2a4cdba89523bea7b775ad659738
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-05-25 07:04:17 +0000

    Add files via upload

---
+++ lib/admin/screens/adminHomePage.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:startupincubator/createPost.dart';
+import 'package:startupincubator/viewComplaints.dart';
+import 'removeuser.dart';
+
+
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight + 20),
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 20),
+            child: AppBar(
+              title: const Text("Hello,Admin"),
+              actions: const [
+                Padding(
+                    padding: EdgeInsets.only(right: 16.0),
+                    child: Icon(
+                      Icons.login,
+                      size: 30.0,
+                    )
+                )
+              ],
+            ),
+          ),
+        ),
+
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.remove,size: 50,),
+                title: const Text('Remove Users'),
+                onTap: () {
+                  // Handle drawer item tap
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>RemoveUser()));
+                  // Navigator.pop(context); // Close the drawer
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.create,size: 40,),
+                title: const Text('Create Post'),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>CreatePost()));
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.view_agenda,),
+                title: const Text('Complaints'),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Complaints()));
+                },
+              ),
+            ],
+          ),
+        ),
+        body: SafeArea(
+          child: Container(
+            color: Colors.white10,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        "Your Posts",
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "poppins"
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0,10),
+                    child: Container(
+                      width: 390,
+                      height: 147,
+                      decoration: BoxDecoration(
+                          color: Colors.white
+                      ),
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+                        child: InkWell(
+                          splashColor: Colors.transparent,
+                          focusColor: Colors.transparent,
+                          hoverColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          onTap: () async {},
+                          child: Text("Message"),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Container(
+                    width: 390,
+                    height: 147,
+                    decoration: const BoxDecoration(
+                        color: Colors.white
+
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+                      child: InkWell(
+                        splashColor: Colors.transparent,
+                        focusColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        onTap: () async {},
+                        child: const Text(
+                          'Message',
+
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Container(
+                    width: 390,
+                    height: 147,
+                    decoration: const BoxDecoration(
+                        color: Colors.white
+
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+                      child: InkWell(
+                        splashColor: Colors.transparent,
+                        focusColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        onTap: () async {},
+                        child: const Text(
+                          'Message',
+
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/admin/screens/removeuser.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:startupincubator/userModel.dart';
+
+class RemoveUser extends StatefulWidget {
+  const RemoveUser({Key? key}) : super(key: key);
+
+  @override
+  State<RemoveUser> createState() => _RemoveUserState();
+}
+
+class _RemoveUserState extends State<RemoveUser> {
+  final myController = TextEditingController();
+  final List<Item> itemList = [
+    Item(title: 'userName', subtitle: 'Role'),
+    Item(title: 'userName', subtitle: 'Role'),
+    Item(title: 'userName', subtitle: 'Role'),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.indigoAccent[700],
+
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_rounded,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: const Text(
+              "Remove User",
+            style: TextStyle(),
+
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(20.0),
+            child: Container(),
+          ),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(16, 4, 16, 0),
+              child: TextFormField(
+                controller: myController,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.search_outlined),
+                  border: InputBorder.none,
+                  labelText: "Enter StartUp Name",
+
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            const Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(16, 12, 0, 0),
+              child: Text(
+                'Startups matching search',
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: itemList.length,
+                  itemBuilder: (context,index) {
+                    final item=itemList[index];
+                    return ListTile(
+                      title: Text(
+                        item.title,
+                        style: const TextStyle(fontSize: 40),
+                      ),
+                      subtitle: Text(item.subtitle),
+                      trailing: const Icon(Icons.delete),
+                    );
+                  },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
