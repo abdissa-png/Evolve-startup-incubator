e972b56673d2813afc783d65745c0d4aeb3f724b
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-05-28 07:28:49 +0000

    Add files via upload

---
+++ lib/admin/models/complaintModel.dart
@@ -0,0 +1,5 @@
+class Complaint {
+  String userName;
+  String subject;
+  Complaint({required this.userName, required this.subject});
+}
---
+++ lib/admin/models/removeUserModel.dart
@@ -0,0 +1,8 @@
+// I write this as model we can put the actual field inside the class item
+
+class DeleteUser {
+  final String title;
+  final String subtitle;
+
+  DeleteUser({required this.title, required this.subtitle});
+}
