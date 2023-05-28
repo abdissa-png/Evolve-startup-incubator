b2a2aa3d6b31ca53d8fe28890661295442cdc876
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-05-28 07:23:35 +0000

    Add files via upload

--- lib/admin/screens/viewComplaints.dart
+++ lib/admin/screens/viewComplaints.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'complaintModel.dart';
 
-class Complaints extends StatelessWidget {
+class Complaints extends StatefulWidget {
   const Complaints({Key? key}) : super(key: key);
 
+  @override
+  State<Complaints> createState() => _ComplaintsState();
+}
+
+class _ComplaintsState extends State<Complaints> {
+  final List<Complaint> complaints = [
+    Complaint(
+        userName: "username",
+        subject:
+            "MessageMejfdjflajldjal;dljfldjflsa;lsljdlfjdlkfjlkdjflejifjsajdkfjldkjflkdjfljdklfjdj"),
+    Complaint(
+        userName: "userName",
+        subject:
+            "MessageMessageMejfdjflajldjal;dljfldjflsa;lsljdlfjdlkfjlkdjflejifjsajdkfjldkjflkdjfljdklfjdj"),
+    Complaint(
+        userName: "userName",
+        subject:
+            "MessageMessageMejfdjflajldjal;dljfldjflsa;lsljdlfjdlkfjlkdjflejifjsajdkfjldkjflkdjfljdklfjdj"),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.indigoAccent[700],
-
           leading: IconButton(
             icon: const Icon(
               Icons.arrow_back_rounded,
@@ -21,72 +40,95 @@ class Complaints extends StatelessWidget {
           ),
           title: const Text("Complaints"),
         ),
-        body: const Column(
-          children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                        "User name",
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    ),
-
-                    Icon(
-                      Icons.arrow_drop_down,
-                      size: 50,
-
-                    )
-                  ],
+        body: ListView.builder(
+            itemCount: complaints.length,
+            itemBuilder: (context, index) {
+              final complaint = complaints[index];
+              return ExpansionTile(
+                title: Text(
+                  complaint.userName,
+                  style: const TextStyle(fontSize: 40),
                 ),
-                Row(
-                  children: [
-                    Text(
-                      "Message Subject",
-                      style: TextStyle(
-                        // fontSize: 30,
-                        fontFamily: "poppins"
-                      ),
-                    ),
-
-                  ],
+                subtitle: Text(complaint.subject.substring(0, 7)),
+                trailing: const Icon(
+                  Icons.arrow_drop_down,
+                  size: 50,
                 ),
-                SizedBox(height: 20,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "User name",
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    ),
-                    Icon(
-                      Icons.arrow_drop_down,
-                      size: 50,
-
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "Message Subject",
-                      style: TextStyle(
-
-                          fontFamily: "poppins"
-                      ),
-                    ),
-
-                  ],
-                )
-              ],
-            )
-          ],
-        ),
+                children: [
+                  Text(complaint.subject),
+                  // Expanded(child: ElevatedButton(
+                  //     onPressed: (){},
+                  //   child: const Text("Reply"),
+                  // ),
+                ],
+              );
+            }),
+        // body: const Column(
+        //   children: [
+        //     Column(
+        //       children: [
+        //         Row(
+        //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //           children: [
+        //             Text(
+        //                 "User name",
+        //               style: TextStyle(
+        //                 fontSize: 30,
+        //               ),
+        //             ),
+        //
+        //             Icon(
+        //               Icons.arrow_drop_down,
+        //               size: 50,
+        //
+        //             )
+        //           ],
+        //         ),
+        //         Row(
+        //           children: [
+        //             Text(
+        //               "Message Subject",
+        //               style: TextStyle(
+        //                 // fontSize: 30,
+        //                 fontFamily: "poppins"
+        //               ),
+        //             ),
+        //
+        //           ],
+        //         ),
+        //         SizedBox(height: 20,),
+        //         Row(
+        //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //           children: [
+        //             Text(
+        //               "User name",
+        //               style: TextStyle(
+        //                 fontSize: 30,
+        //               ),
+        //             ),
+        //             Icon(
+        //               Icons.arrow_drop_down,
+        //               size: 50,
+        //
+        //             )
+        //           ],
+        //         ),
+        //         Row(
+        //           children: [
+        //             Text(
+        //               "Message Subject",
+        //               style: TextStyle(
+        //
+        //                   fontFamily: "poppins"
+        //               ),
+        //             ),
+        //
+        //           ],
+        //         )
+        //       ],
+        //     )
+        //   ],
+        // ),
       ),
     );
   }
